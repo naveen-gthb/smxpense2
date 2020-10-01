ea03935fb21361be552658fc764aca2f1dcf04af
naveen-gthb <11699nk@gmail.com>
2020-10-02 03:50:28 +0530

    SMSXpense

---
+++ lib/pages/monthly.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+import 'package:intl/intl.dart';
+
+class Monthly extends StatefulWidget {
+  @override
+  _MonthlyState createState() => _MonthlyState();
+}
+
+class _MonthlyState extends State<Monthly> {
+  Map<String, double> dataMap = {
+    "Week 1": 2,
+    "Week 2": 3,
+    "Week 3": 5,
+    "Week 4": 2,
+  };
+
+  DateTime _selectedDate;
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+    print('...');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Monthly Expenses"),
+        backgroundColor: Color(0xFF5e17eb),
+      ),
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(15),
+              width: double.infinity,
+              //height: 200,
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Monthly()),
+                  );
+                },
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(
+                        top: 15,
+                        left: 15,
+                      ),
+                      child: Text(
+                        "Today",
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            padding:
+                                EdgeInsets.only(left: 15, top: 5, right: 15),
+                            child: Text(
+                              "₹100",
+                              style: TextStyle(
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                color: Color(0xFF5e17eb),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 2,
+            child: PieChart(
+              dataMap: dataMap,
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.only(left: 15, right: 15),
+              width: double.infinity,
+              height: 70,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      padding: EdgeInsets.all(15),
+                      color: Color(0xFF5e17eb),
+                      textColor: Colors.white,
+                      child: Text(
+                        _selectedDate == null
+                            ? 'No Date Chosen!'
+                            : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
+                      ),
+                      onPressed: _presentDatePicker,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/today.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/new_transaction.dart';
+import '../widgets/transaction_list.dart';
+import '../models/transaction.dart';
+
+class Daily extends StatefulWidget {
+  @override
+  _DailyState createState() => _DailyState();
+}
+
+class _DailyState extends State<Daily> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  List<Transaction> get recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
+  void addNewTransaction(String txTitle, double txAmount, DateTime chosenDate) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: chosenDate,
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
+  void _deleteTransaction(String id) {
+    setState(() {
+      _userTransactions.removeWhere((tx) => tx.id == id);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Transform.scale(
+        scale: 1.1,
+        child: FloatingActionButton(
+          onPressed: () => _startAddNewTransaction(context),
+          backgroundColor: Color(0xFF5e17eb),
+          child: Image(
+            image: AssetImage('assets/Money.png'),
+            width: 30,
+          ),
+        ),
+      ),
+      appBar: AppBar(
+        title: Text("Today's Expenses"),
+        backgroundColor: Color(0xFF5e17eb),
+      ),
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(15),
+              width: double.infinity,
+              //height: 200,
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Daily()),
+                  );
+                },
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(
+                        top: 15,
+                        left: 15,
+                      ),
+                      child: Text(
+                        "Today",
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            padding:
+                                EdgeInsets.only(left: 15, top: 5, right: 15),
+                            child: Text(
+                              "₹100",
+                              style: TextStyle(
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                color: Color(0xFF5e17eb),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(left: 15, right: 15),
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    //Chart(_recentTransactions),
+                    TransactionList(_userTransactions, _deleteTransaction),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
